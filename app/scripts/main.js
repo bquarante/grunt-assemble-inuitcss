e6c5f27184de2fd6f9cfcff73fad1f23d24992df
b40 <bquarante@gmail.com>
2014-02-13 15:20:46 +0100

    initial commit on branche debug
    thomas' help

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,21 @@
-require.config({
-    paths: {
-        jquery: '../bower_components/jquery/jquery'
-    }
-});
-
-require(['app', 'jquery'], function (app, $) {
-    'use strict';
-    // use app here
-    console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+$(function() {
+	'use strict';
+	$('.rslides').responsiveSlides({
+		auto: true,// Boolean: Animate automatically, true or false
+		speed: 1000,// Integer: Speed of the transition, in milliseconds
+		timeout: 5000,// Integer: Time between slide transitions, in milliseconds
+		pager: true,// Boolean: Show pager, true or false
+		nav: false,// Boolean: Show navigation, true or false
+		random: false,// Boolean: Randomize the order of the slides, true or false
+		pause: false,// Boolean: Pause on hover, true or false
+		pauseControls: true,// Boolean: Pause when hovering controls, true or false
+		prevText: 'Previous',// String: Text for the 'previous' button
+		nextText: 'Next',// String: Text for the 'next' button
+		maxwidth: '',// Integer: Max-width of the slideshow, in pixels
+		navContainer: '',// Selector: Where controls should be appended to, default is after the 'ul'
+		manualControls: '.thumbs',// Selector: Declare custom pager navigation
+		namespace: 'rslides',// String: Change the default namespace used
+		before: function(){},// Function: Before callback
+		after: function(){}// Function: After callback
+	});
 });
